65baab9052df4db4ac9f4ef504ae52d0a1284037
Flavio-LP <flavio5070@outlook.com>
2025-05-05 21:37:50 -0300

    inserido rotas dos carros

--- app/controllers/carros_controller.rb
+++ app/controllers/carros_controller.rb
@@ -1,17 +1,48 @@
 class CarrosController < ApplicationController
+  def index
+    @carros = Carro.all
+  end
+  def new
+    @carro = Carro.new
+  end
 
-    def index
-        @carros = Carro.todos
+  def show
+    @carro = Carro.find(params[:id])
+  end
+
+
+
+  def create
+    @carro = Carro.new()
+    if @carro.save
+      redirect_to @carro, notice: "Carro criado com sucesso!"
+    else
+      render :new
     end
+  end
 
-    def show
-        carros = Carro.todos
+  def edit
+    @carro = Carro.find(params[:id])
+  end
 
-        id = params[:id].to_i
-        @carro = carros.find { |carro| carro.id == id }
-        if @carro.nil?
-            raise ActionController::RoutingError.new('Not Found')
-        end
+  def update
+    @carro = Carro.find(params[:id])
+    if @carro.update(carro_params)
+      redirect_to @carro, notice: "Carro atualizado com sucesso!"
+    else
+      render :edit
     end
+  end
 
+  def destroy
+    @carro = Carro.find(params[:id])
+    @carro.destroy
+    redirect_to carros_path, notice: "Carro excluído com sucesso!"
+  end
+
+  private
+
+  def carro_params
+    params.require(:carro).permit(:marca, :modelo, :ano)
+  end
 end
